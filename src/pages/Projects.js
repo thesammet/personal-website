256daf8f297e439ccaf1743180a1a4823611eaf2
Abdü Samed AKGÜL <50338990+thesammet@users.noreply.github.com>
2022-09-12 03:56:48 +0300

    addition: projects
    projects page with lorem project

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import ProjectItem from '../components/ProjectItem'
+import { ProjectList } from '../helpers/ProjectList'
+import '../styles/Projects.css'
 
 function Projects() {
     return (
-        <div>Projects</div>
+
+        <div className='projects'>
+            <h1>Personal Projects</h1>
+            <div className='projectList'>
+                {ProjectList.map((item, index) => {
+                    return <ProjectItem id={index} name={item.name} image={item.image} />
+                })}
+            </div>
+        </div>
     )
 }
 
